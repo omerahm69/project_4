cdd57fd575ed095e2a3a0c373e39bfb3a7a45812
Omer Ahmed <omerahm69@gmail.com>
2025-01-25 15:03:34 +0000

    solved an error in database connectio

--- my_restaurant/views.py
+++ my_restaurant/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
+from my_restaurant import views as home_views
 from django.http import HttpResponse
 #from django.views import generic
 
 
 
 # Create your views here.
-def home(request):
+def home_views(request):
         return HttpResponse(request, 'This is my home page')
