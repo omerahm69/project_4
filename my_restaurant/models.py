6d5e2685e08aa500cbcc496fddba021f5e6b8fef
omer ahmed <omerahm69@gmail.com>
2025-02-08 23:02:20 +0100

    added dishes in menu

--- my_restaurant/models.py
+++ my_restaurant/models.py
@@ -22,6 +22,7 @@ class Customer(models.Model):
 class MenuItem(models.Model):
     name=models.CharField(max_length=100)
     description=models.TextField()
+    
     price=models.DecimalField(max_digits=6, decimal_places=2)
     available=models.BooleanField(default=True)
 
