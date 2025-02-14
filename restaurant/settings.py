26c8f30a051617e0196ced6ab321348e0c1bb815
omer ahmed <omerahm69@gmail.com>
2025-02-15 00:00:17 +0100

    static files

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -133,12 +133,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
+BASE_DIR = Path(__file__).resolve().parent.parent
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATIC_URL = '/static/'
 
-STATIC_URL = ' static/'
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static")
-]
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT=os.path.join(BASE_DIR, 'media')
@@ -148,7 +146,8 @@ MEDIA_ROOT=os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
-STATICFILES_DIRS =(os.path.join(BASE_DIR,'static'),)
+STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
 
 django_heroku.settings(locals())
