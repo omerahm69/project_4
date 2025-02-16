6defff7e715752e9a9536631f116fd150fd101da
omer ahmed <omerahm69@gmail.com>
2025-02-16 22:11:04 +0100

    fixed images isuues when deploying

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -118,6 +118,8 @@ STATIC_URL = '/static/'
 MEDIA_URL = 'media/'
 MEDIA_ROOT=os.path.join(BASE_DIR, 'media')
 
+if 'DYNO' in os.environ:
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
