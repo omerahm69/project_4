dea7111182918d2d7854c95bfa4b016a557911be
omer ahmed <omerahm69@gmail.com>
2025-05-15 21:11:35 +0200

    Update settings.py

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -27,7 +27,7 @@ TEMPLATES_DIR=os.path.join(BASE_DIR,'templates')
 SECRET_KEY = 'django-insecure-!+oz1)35@#_90iyon!k4d98++mi@92x5%8#_qfeuo5_2%!=t&%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
